a29a99fcb2421399c08cec64416acbb2c843ff07
Puja Patel <Puja@Patricks-MacBook-Pro.local>
2021-06-01 23:54:47 -0700

    working towards drawing polygons

---
+++ src/graphJSON.tsx
@@ -0,0 +1,26 @@
+//import inputData from "./inputData.json";
+
+export interface JSONforGraph {
+    vertices: number[][];
+    edges: number[][];
+    polygons: number[][];
+}
+
+// export function fromJsontoVerticesEdges(){
+//     let inputGraph: JSONforGraph = {vertices: inputData.vertices, edges: inputData.edges};
+//     console.log("starting graph info")
+//     console.log(inputGraph);
+
+//     return inputGraph;
+//   }
+
+
+//   export function fromJsontoVerticesEdgesPolygons(json: JSONforGraph){
+//     // //current graph
+//     let inputGraph: JSONforGraph = {vertices: json.vertices, edges: json.edges, polygons: json.polygons };
+//     // console.log(inputGraph);
+
+//     return inputGraph;
+//   }
+
+
